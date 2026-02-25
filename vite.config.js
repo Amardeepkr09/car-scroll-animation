350c42da044636098399bc1210984a63405706d1
amardeep kumar <amardeepkr1962003@gmail.com>
2026-02-25 18:30:09 +0530

    fix: update vite config, add SPA fallback

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // Import this
 
 export default defineConfig({
-  base: '/', // root deploy
-  plugins: [react()],
-});
+  base: '/',
+  plugins: [
+    react(),
+    tailwindcss(), // Add this to the plugins array
+  ],
+})
